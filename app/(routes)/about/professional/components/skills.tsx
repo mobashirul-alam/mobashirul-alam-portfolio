b462b7e214fb49a0ca66580ffd193c50ffee94fb
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-02-22 13:18:03 +0600

    Skill section added

--- app/(routes)/about/professional/components/skills.tsx
+++ app/(routes)/about/professional/components/skills.tsx
@@ -1,6 +1,58 @@
-import { RiCloseLine } from "@remixicon/react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+    RiBootstrapLine,
+    RiCloseLine,
+    RiCss3Fill,
+    RiGithubFill,
+    RiHtml5Fill,
+    RiJavascriptFill,
+    RiReactjsFill,
+    RiTrelloFill,
+} from "@remixicon/react";
+import { BiLogoNetlify, BiLogoTypescript } from "react-icons/bi";
+import { SiFirebase, SiPostman, SiTailwindcss, SiVercel } from "react-icons/si";
+import { TbBrandNextjs, TbBrandRedux, TbBrandVscode } from "react-icons/tb";
 
 const Skills = () => {
+    const languageSkills = [
+        {
+            id: 1,
+            name: "HTML",
+            icon: RiHtml5Fill,
+        },
+        {
+            id: 2,
+            name: "CSS",
+            icon: RiCss3Fill,
+        },
+        {
+            id: 3,
+            name: "JavaScript",
+            icon: RiJavascriptFill,
+        },
+        {
+            id: 4,
+            name: "TypeScript",
+            icon: BiLogoTypescript,
+        },
+    ];
+
+    const libraryFrameworks = [
+        { id: 1, name: "React", icon: RiReactjsFill },
+        { id: 2, name: "Next.js", icon: TbBrandNextjs },
+        { id: 3, name: "Redux", icon: TbBrandRedux },
+        { id: 4, name: "Tailwind CSS", icon: SiTailwindcss },
+        { id: 5, name: "Bootstrap", icon: RiBootstrapLine },
+    ];
+    const tools = [
+        { id: 1, name: "GitHub", icon: RiGithubFill },
+        { id: 2, name: "VS Code", icon: TbBrandVscode },
+        { id: 3, name: "Firebase", icon: SiFirebase },
+        { id: 4, name: "Netlify", icon: BiLogoNetlify },
+        { id: 5, name: "Vercel", icon: SiVercel },
+        { id: 6, name: "Postman", icon: SiPostman },
+        { id: 7, name: "Trello", icon: RiTrelloFill },
+    ];
     return (
         <div className="w-full border-r">
             <div className="border-b">
@@ -9,9 +61,82 @@ const Skills = () => {
                     <RiCloseLine />
                 </div>
             </div>
-            <div className="p-5">
-                <p>Skills</p>
-            </div>
+            <ScrollArea className="h-96">
+                <div className="p-5">
+                    <div className="mb-5">
+                        <h1 className="text-base font-medium text-white mb-2">
+                            Languages
+                        </h1>
+                        <div className="flex items-center gap-5">
+                            {languageSkills.map((skill) => (
+                                <div
+                                    key={skill.id}
+                                    className="border hover:border-white/50 shadow-md py-3 rounded-md flex flex-col justify-center items-center w-36 duration-1000"
+                                >
+                                    <skill.icon size={32} className="mb-1" />
+                                    <p className="text-lg font-semibold">
+                                        {skill.name}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="mb-5">
+                        <h1 className="text-base font-medium text-white mb-2">
+                            Libraries & Frameworks
+                        </h1>
+                        <div className="flex flex-wrap items-center gap-5">
+                            {libraryFrameworks.map((skill) => (
+                                <div
+                                    key={skill.id}
+                                    className="border hover:border-white/50 shadow-md py-3 rounded-md flex flex-col justify-center items-center w-36 duration-1000"
+                                >
+                                    <skill.icon size={32} className="mb-1" />
+                                    <p className="text-lg font-semibold">
+                                        {skill.name}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="mb-5">
+                        <h1 className="text-base font-medium text-white mb-2">
+                            Tools & Platforms
+                        </h1>
+                        <div className="flex flex-wrap items-center gap-5">
+                            {tools.map((skill) => (
+                                <div
+                                    key={skill.id}
+                                    className="border hover:border-white/50 shadow-md py-3 rounded-md flex flex-col justify-center items-center w-36 duration-1000"
+                                >
+                                    <skill.icon size={32} className="mb-1" />
+                                    <p className="text-lg font-semibold">
+                                        {skill.name}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="mb-5">
+                        <h1 className="text-base font-medium text-white mb-2">
+                            Tools & Platforms
+                        </h1>
+                        <div className="flex flex-wrap items-center gap-5">
+                            {tools.map((skill) => (
+                                <div
+                                    key={skill.id}
+                                    className="border hover:border-white/50 shadow-md py-3 rounded-md flex flex-col justify-center items-center w-36 duration-1000"
+                                >
+                                    <skill.icon size={32} className="mb-1" />
+                                    <p className="text-lg font-semibold">
+                                        {skill.name}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </ScrollArea>
         </div>
     );
 };
